ee3821970d7c201ece76325a5c596b4c157d7a22
Shashank Parashar <shashank.parashar@velocis.in>
2026-01-14 15:44:31 +0530

    Initial Commit

---
+++ app/routes/about.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const about = () => {
+  return (
+    <h1 className="text-3xl font-bold text-red-300">
+      Hi Welcome to router declarative mode
+    </h1>
+  );
+};
+
+export default about;
---
+++ app/routes/about/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AboutPage = () => {
+  return (
+    <h1 className="text-3xl font-bold text-red-300">
+      Hi Welcome to router declarative mode
+    </h1>
+  );
+};
+
+export default AboutPage;
